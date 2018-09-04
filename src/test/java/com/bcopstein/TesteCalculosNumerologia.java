2315fcdbdb47bffc7b8d275eedf728f5ae061547
Bernardo Copstein <bernardo.copstein@pucrs.br>
2018-09-04 11:14:01 -0300

    novo pom com JUnit 5

--- src/test/java/com/bcopstein/TesteCalculosNumerologia.java
+++ src/test/java/com/bcopstein/TesteCalculosNumerologia.java
@@ -1,8 +1,8 @@
 package com.bcopstein;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
 
 import org.junit.Before;
 import org.junit.Test;
